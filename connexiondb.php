1b822a81b3dfe1ef0dd23debad04596961388609
soumia elyakni <selyakni@gmail.com>
2022-04-15 09:25:27 +0000

    classConexion

--- connexiondb.php
+++ connexiondb.php
@@ -1,26 +1,15 @@
 <?php
 
+class connexion {
+    function __construct() {
+$connect = new mysqli("localhost", "root", "", "gestioncontacts");
+if($connect -> connect_error) {
+  die("connection failed :".$connect -> connect_error);
+}
 
-$servername = 'localhost';
-$username = 'root';
-$password = '';
 
-//On établit la connexion
-
-            //On essaie de se connecter
-            try{
-                $conn = new PDO("mysql:host=.$servername.;dbname=gestioncontacts; charset=utf8", $username, $password);
-                //On définit le mode d'erreur de PDO sur Exception
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                echo 'Connexion réussie';
-            }
-            
-            /*On capture les exceptions si une exception est lancée et on affiche
-             *les informations relatives à celle-ci*/
-            catch(PDOException $e){
-              echo "Erreur : " . $e->getMessage();
-            } 
-
+}
+}
 
 ?>
 
