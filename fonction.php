7e43d5545fc04a9bdfa3a5026504bfcd7b64dabb
soumia elyakni <selyakni@gmail.com>
2022-04-17 18:20:21 +0000

    functExtendUser

--- fonction.php
+++ fonction.php
@@ -6,7 +6,7 @@ require_once 'contact.php';
 
 session_start(); 
 
-class funct {
+class funct extends users {
     function __construct() {  
         
         // creat connexion object 
@@ -14,55 +14,9 @@ class funct {
            
     } 
 
-    // function de signUp
+    }
 
 
 
-    public function Login($username, $password){ 
-                    $conn = $this->db->connect(); 
-                    $res = mysqli_query($conn, "SELECT * FROM users WHERE username = '".$username."' AND password = '".md5($password)."'");  
-                    $user_data = mysqli_fetch_array($res);  
-
-                    // print_r($user_data);  
-                    $no_rows = mysqli_num_rows($res);  
-                      
-                    if ($no_rows == 1)   
-                    {  
-                   
-                        $_SESSION['login'] = true;  
-                        $_SESSION['id'] = $user_data['id'];  
-                        $_SESSION['username'] = $user_data['username'];  
-                         
-                        return TRUE;  
-                    }  
-                    else  
-                    {  
-                        return FALSE;  
-                    }  
-                } 
-    public function addNewUser($username, $password){  
-                    $conn = $this->db->connect();
-                    
-                    $password = md5($password);  
-                    $qr = mysqli_query($conn, "INSERT INTO users(username, password) values('$username', '$password')") or die(); 
-                return $qr;
-       
-            }  
-
-    public function isUserExist($username){  
-        $conn = $this->db->connect();
-
-        $qr = mysqli_query($conn, "SELECT * FROM users WHERE username = ".$username."");  
-        echo $row = mysqli_num_rows($qr);  
-        if($row > 0){  
-            return true;  
-        } else { 
-            echo 'no' ;
-            return false;  
-        }  
-           }
-
-
-}
 
 ?>
--- users.php
+++ users.php
@@ -6,6 +6,7 @@ class users {
     public $username;
     public $password;
     public $id;
+    public $signupdate;
 
     public function __construct($username, $password)
     {
@@ -14,21 +15,60 @@ class users {
         
     }
 
+    // function de Login
 
-    // public function controll($username){
-    //     if (count($this->username)==0){
-    //         return true;
-    //     }
-    //     foreach($this->users as $user){
-    //         if($user->getuser()==$username){
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
+                public function Login($username, $password){ 
+                    $conn = $this->db->connect(); 
+                    $res = mysqli_query($conn, "SELECT * FROM users WHERE username = '".$username."' AND password = '".md5($password)."'");  
+                    $user_data = mysqli_fetch_array($res);  
+                
+                    // print_r($user_data);  
+                    $no_rows = mysqli_num_rows($res);  
 
-}
+                    if ($no_rows == 1)   
+                    {  
+                    
+                        $_SESSION['login'] = true;  
+                        $_SESSION['id'] = $user_data['id'];  
+                        $_SESSION['username'] = $user_data['username'];
+                        $_SESSION['datesignup'] = $user_data['datesignup'];
+
+                        return TRUE;  
+                    }  
+                    else  
+                    {  
+                        return FALSE;  
+
+                    }  
+                } 
+            
+    //   fonction de registrartion
+
+                public function isUserExist($username){  
+                         $conn = $this->db->connect();
+                
+                
+                         $qr = mysqli_query($conn, "SELECT * FROM `users` WHERE `username` = '".$username."'");  
+                         $row = mysqli_num_rows($qr);  
+                         if($row > 0){  
+                             return true;  
+                         } else { 
+                             return false;  
+                         }  
+                     
+                }  
+
+                public function addNewUser($username, $password){  
+                        $conn = $this->db->connect();
+
+                        $password = md5($password);  
+                        $qr = mysqli_query($conn, "INSERT INTO users(username, password) values('$username', '$password')") or die(); 
+                    return $qr;
+                
+                }  
+
+                }
 
 
 
-?>
+    ?>
