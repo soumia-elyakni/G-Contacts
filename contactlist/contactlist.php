ea92e5fdd411b5722872a3ef0ceb91c7e0075c02
soumia elyakni <selyakni@gmail.com>
2022-04-19 09:36:08 +0000

    phpBalise

--- contactlist/contactlist.php
+++ contactlist/contactlist.php
@@ -1,3 +1,21 @@
+<?php
+
+include_once '../fonction.php';
+
+if(!isset($_SESSION['login'])){
+  
+   header("location: ../index.php");
+   
+   }
+
+  //  $username = $_SESSION['username'] ;
+  //  $signupdate = $_SESSION['datesignup'] ;
+  //  $lastlogin = ;
+
+   ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -48,12 +66,12 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
         <div class="container-fluid">
-        <a class="navbar-brand text-light" href="#">Navbar</a>
+        <a class="navbar-brand text-light" href="../profil.php">Profil</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <a class="nav-link active text-light" aria-current="page" href="#">login</a>
+                <a class="nav-link active text-light" aria-current="page" href="../logouat.php">Logouat</a>
         </div>
         </div>
     </nav>
@@ -61,7 +79,7 @@
 
     <div class="d-flex align-items-center justify-content-between m-5">
         <h1>Contacts List :</h1>
-       <button class=" btn bg-secondary text-white " id="modaladd"> Add</button>
+       <button class=" btn bg-secondary text-white " id="modaladd" data-bs-toggle="modal" data-bs-target="#staticBackdrop" > Add</button>
     </div>
     <table class="table ">
         <thead>
@@ -74,22 +92,6 @@
         </thead>
         <tbody>
           <tr>
-            <th scope="row">Alonzo E Barber</th>
-            <td>alonzo@barber.com</td>
-            <td>818-740-3656</td>
-            <td>848 Glendale Avenue LOs angekes california 90017 </td>
-            <td><a class="a" href="edit.html">Edit</a></td>
-            <td><a class="a" href="">Delete</a></td>
-          </tr>
-          <tr>
-            <th scope="row">Annmarie</th>
-            <td>annmarie@nreeves.com</td>
-            <td>413-626-0746</td>
-            <td>3984 Leverton Cove Road Springfield Massachuetts 01109</td>
-            <td><a class="a" href="edit.html">Edit</a></td>
-            <td><a class="a" href="">Delete</a></td>
-          </tr>
-          <tr>
             <th scope="row">Raouya</th>
             <td>raouya@raouya.com</td>
             <td>123-456-6789</td>
@@ -99,12 +101,15 @@
           </tr>
         </tbody>
       </table>
-      <div class="container mar">
-        <div class="model hidden">
-        <div class="modal-content border-none">
-        <span class="close ">&times;</span>
+
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title m-auto" id="staticBackdropLabel">Add New Contact</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="container w-75 bg-white" >
+      <div class="modal-body">
       <form class="d-flex flex-column" method="POST" name="addform" id="addform">
         <label class="form-label">name</label>
         <input type="text"  class="form-control " id="name" name="name" placeholder="Entrer le nom">
@@ -118,11 +123,16 @@
         <label class="form-label">Adresse</label>
         <input type="Adresse"  class="form-control " id="adress"  name="enroll" placeholder="Enter Adresse">
         <small id="messageadress" class="text-danger "></small>
-        <input class=" btn bg-secondary text-white m-5 p-2 " type="submit" value="add" name="save" >  
+        <input class=" btn bg-secondary text-white mt-5 p-2 w-100 " type="submit" value="add" name="save" >  
     </form>
+
+      </div>
+     
+    </div>
   </div>
+</div>
   </main>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<script src="modaladd.js"></script>
+<!-- <script src="modaladd.js"></script> -->
 </body>
 </html>
